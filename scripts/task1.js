3124b407830b777e9e1902908d8a31c0c0141af2
Andriana Popp <115152248+andrianapopp@users.noreply.github.com>
2023-03-10 21:24:37 +0200

    hw6

---
+++ scripts/task1.js
@@ -0,0 +1,13 @@
+console.log(Boolean(Number(`10`)) + 1);
+// 2
+console.log(`sub` + Number(false));
+// sub0
+//false=0
+console.log(16 * `91`);
+//1456
+console.log(true-70);
+//-69
+//true=1
+console.log(Number(1 + String(1)) + 1);
+//1 + String(1)=11
+//Number(11) + 1=12
